5408d3f83bf2501b93ba871348e9a2dcfee73646
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-21 13:10:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -218,7 +218,7 @@ end
     teams[:players].each do |player,data|
       if data[:points] > points_scored
         points_scored = data[:points]
-        value_to_return = data
+        value_to_return = data.key(points_scored)
       end
     end
   end
