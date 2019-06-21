8595e4087290c68e15d9c5a1c358ecbb56b6c7cc
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-21 13:37:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -262,18 +262,7 @@ end
 end
 
  def long_name_steals_a_ton?
-  value_to_return = 0
-  nameLen = 0
-  hash = game_hash
-  game_hash.each do |sides,teams|
-    teams[:players].each do |player,data|
-      if player.to_s.length > nameLen
-        nameLen = player.to_s.length
-        value_to_return = player
-      end
-    end
-  end
-  value_to_return
+  return player_with_longest_name
 end
 
 
